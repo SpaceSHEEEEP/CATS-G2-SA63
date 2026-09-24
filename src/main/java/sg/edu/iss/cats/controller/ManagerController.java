38927b63994fd52086ce8e6f2aa0555e61fd24b5
Mikhail Mahamood <mikhailmahamood@gmail.com>
2026-09-24 21:12:59 +0800

    first commit

---
+++ src/main/java/sg/edu/iss/cats/controller/ManagerController.java
@@ -0,0 +1,11 @@
+package sg.edu.iss.cats.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller 
+@RequestMapping("/manager") // for manager stuff
+public class ManagerController {
+
+    
+}
---
+++ src/main/java/sg/edu/iss/cats/controller/StaffController.java
@@ -0,0 +1,11 @@
+package sg.edu.iss.cats.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/staff") // for employee's stuff
+public class StaffController {
+
+    
+}
