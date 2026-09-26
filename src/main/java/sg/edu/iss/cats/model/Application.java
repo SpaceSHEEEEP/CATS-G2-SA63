6b6e0c6d48a66f148f14b2a80913d40840ee6507
kelvinleecreations <junronglee01@gmail.com>
2026-09-26 16:51:18 +0800

    added application and applicationstatus

--- src/main/java/sg/edu/iss/cats/model/Application.java
+++ src/main/java/sg/edu/iss/cats/model/Application.java
@@ -1,13 +1,28 @@
 package sg.edu.iss.cats.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
 public class Application {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int applicationId;
-    // TODO: add the rest of the attributes
+    private int courseId;
+    private int userId;
+
+    @Enumerated(EnumType.STRING)
+    private ApplicationStatus applicationStatus;
+    private String userReason;
+    private String managerReason;
+    private Boolean hasBeenPaid;
 }
